a985c3e9da33797d1d5aa209ff83009e6d14c526
Sebastian Retamal <retamal.sebastian@gmail.com>
2023-12-15 10:33:56 -0500

    first commit

---
+++ src/main/java/com/srm/nisumChallenge/NisumChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.srm.nisumChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumChallengeApplication.class, args);
+	}
+
+}
